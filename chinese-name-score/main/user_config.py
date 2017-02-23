b3d05db69a9fb7ef36020ec26bcfd207be47eda4
lynxme <zhoulingfeng@126.com>
2017-02-23 19:24:53 +0800

    qqq

---
+++ chinese-name-score/main/user_config.py
@@ -0,0 +1,34 @@
+# coding:GB18030
+
+"""
+在这里写好配置
+"""
+
+setting = {}
+
+# 限定字，如果配置了该值，则会取用单字字典，否则取用多字字典
+setting["limit_world"] = "辰"
+# 姓
+setting["name_prefix"] = "周"
+# 性别，取值为 男 或者 女
+setting["sex"] = "女"
+# 省份
+setting["area_province"] = "江苏"
+# 城市
+setting["area_region"] = "无锡"
+# 出生的公历年份
+setting['year'] = "2017"
+# 出生的公历月份
+setting['month'] = "2"
+# 出生的公历日子
+setting['day'] = "6"
+# 出生的公历小时
+setting['hour'] = "7"
+# 出生的公历分钟
+setting['minute'] = "38"
+# 结果产出文件名称
+setting['output_fname'] = "names_girls_source_wxy.txt"
+
+
+
+
